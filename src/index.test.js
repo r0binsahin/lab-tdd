37be33dcb0c3a2debc54c87b436fb740444d8eb7
Marcus Rådell <marcus@radell.net>
2024-05-16 14:52:17 +0200

    (test): 0 => Fizzbuzz

--- src/index.test.js
+++ src/index.test.js
@@ -1,3 +1,21 @@
-test('it works', () => {
-  expect(1).toEqual(1);
+// Fizzbuzz
+
+// Divisible 3 => "Fizz"
+
+// Divisible 5 => "Buzz"
+
+// 3 and 5 => "Fizzbuzz"
+
+// else => the number itself
+
+// 15 => "Fizzbuzz"
+
+// 0 => "Fizzbuzz"
+
+const fizzbuzz = (number) => {
+  return "Fizzbuzz";
+};
+
+test("0 => 'Fizzbuzz'", () => {
+  expect(fizzbuzz(0)).toEqual("Fizzbuzz");
 });
